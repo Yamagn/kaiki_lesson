38433830cb1f4d1897a663a8ff92ec5e0d61e6cb
Yamagn <syanekenpon@gmail.com>
2018-07-30 16:43:17 +0900

    Add MSE

--- regression2_multi.py
+++ regression2_multi.py
@@ -6,6 +6,10 @@ train = np.loadtxt("click.csv", delimiter=",", skiprows=1)
 train_x = train[:,0]
 train_y = train[:,1]
 
+# 平均二乗誤差
+def MSE(x, y):
+    return (1 / x.shape[0]) * np.sum(y - f(x)) ** 2
+
 # パラメータを初期化
 theta = np.random.rand(3)
 
@@ -31,24 +35,39 @@ train_z = standardize(train_x)
 
 X = to_matrix(train_z)
 
+# 平均二乗誤差の履歴
+errors = []
+
 # 学習率
 ETA = 1e-3
 
 # 誤差の差分
+diffs = 1
 diff = 1
 
 # 学習を繰り返す
+errors.append(MSE(X, train_y))
 error = E(X, train_y)
 while diff > 1e-2:
     # パラメータ更新
     theta = theta - ETA * np.dot(f(X) - train_y, X)
+    errors.append(MSE(X, train_y))
     # 前回の誤差と差分を計算
     current_error = E(X, train_y)
     diff = error - current_error
     error = current_error
 
+    diffs = errors[-2] - errors[-1]
+
 x = np.linspace(-3, 3, 100)
 
 plt.plot(train_z, train_y, "o")
 plt.plot(x, f(to_matrix(x)))
+plt.show()
+
+# 誤差をプロット
+
+x2 = np.arange(len(errors))
+
+plt.plot(x2, errors)
 plt.show()
