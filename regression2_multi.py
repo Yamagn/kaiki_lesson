682dc07365189fb5abd43078e3f2dfe6424c3615
Yamagn <syanekenpon@gmail.com>
2018-07-30 16:35:32 +0900

    Add regression multi

--- regression2_multi.py
+++ regression2_multi.py
@@ -7,12 +7,15 @@ train_x = train[:,0]
 train_y = train[:,1]
 
 # パラメータを初期化
-theta0 = np.random.rand()
-theta1 = np.random.rand()
+theta = np.random.rand(3)
+
+# 学習データの行列を作る
+def to_matrix(x):
+    return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
 
 # 予測関数
 def f(x):
-    return theta0 + theta1 * x
+    return np.dot(x, theta)
 
 # 目的関数
 def E(x, y):
@@ -26,35 +29,26 @@ def standardize(x):
 
 train_z = standardize(train_x)
 
+X = to_matrix(train_z)
+
 # 学習率
 ETA = 1e-3
 
 # 誤差の差分
 diff = 1
 
-# 更新回数
-count = 0
-
 # 学習を繰り返す
-error = E(train_z, train_y)
+error = E(X, train_y)
 while diff > 1e-2:
-    # 更新結果を一時的に変数に保存
-    tmp0 = theta0 - ETA * np.sum((f(train_z) - train_y))
-    tmp1 = theta1 - ETA * np.sum((f(train_z) - train_y) * train_z)
     # パラメータ更新
-    theta0 = tmp0
-    theta1 = tmp1
+    theta = theta - ETA * np.dot(f(X) - train_y, X)
     # 前回の誤差と差分を計算
-    current_error = E(train_z, train_y)
+    current_error = E(X, train_y)
     diff = error - current_error
     error = current_error
-    #ログの出力
-    count += 1
-    log = "{}回目: theta0 = {:.3f}, theta1 = {:.3f}, 差分 = {:.4f}"
-    print(log.format(count, theta0, theta1, diff))
 
 x = np.linspace(-3, 3, 100)
 
 plt.plot(train_z, train_y, "o")
-plt.plot(x, f(x))
+plt.plot(x, f(to_matrix(x)))
 plt.show()
